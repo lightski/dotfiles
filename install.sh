10c3951bfccdc86906a9ad7d25d172a54963cf56
lightski <drleitzke@gmail.com>
2016-03-20 16:11:57 -0500

    Adds new config files and install entries for: x display manager, x key rebind, xmonad, and xmobar. Deletes extra comment from gitconfig.

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/vim ~/.vim
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/xbindkeysrc.scm ~/.xbindkeysrc.scm
+ln -sf ~/.dotfiles/xinitrc ~/.xinitrc
+ln -sf ~/.dotfiles/xmobarrc ~/.xmobarrc
+ln -sf ~/.dotfiles/xmonad.hs ~/.xmonad/xmonad.hs
+ln -sf ~/.dotfiles/Xresources ~/.Xresources
 
 # add custom environment bits to oh-my-zsh
 for fle in $(ls custom_omz); do
---
+++ xbindkeysrc.scm
@@ -0,0 +1,4 @@
+(xbindkey '("XF86Standby") "sudo killall dhcpcd && sudo pm-suspend")
+(xbindkey '("XF86AudioRaiseVolume") "amixer set Master 2dB+ unmute")
+(xbindkey '("XF86AudioLowerVolume") "amixer set Master 2dB- unmute")
+(xbindkey '("XF86AudioMute") "amixer set Master toggle")
