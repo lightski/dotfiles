b882e0e443767673caff29ef05e18803a12875fa
lightski <drleitzke@gmail.com>
2016-03-22 16:28:42 -0500

    Adds media key bindings for deadbeef playback plus xmonad settings for color border and fullscreen.

--- xbindkeysrc.scm
+++ xbindkeysrc.scm
@@ -2,3 +2,7 @@
 (xbindkey '("XF86AudioRaiseVolume") "amixer set Master 2dB+ unmute")
 (xbindkey '("XF86AudioLowerVolume") "amixer set Master 2dB- unmute")
 (xbindkey '("XF86AudioMute") "amixer set Master toggle")
+(xbindkey '("XF86AudioPlay") "deadbeef --play-pause")
+(xbindkey '("XF86AudioNext") "deadbeef --next")
+(xbindkey '("XF86AudioPrev") "deadbeef --prev")
+(xbindkey '("XF86AudioStop") "deadbeef --stop")
--- xmonad.hs
+++ xmonad.hs
@@ -1,8 +1,11 @@
 import XMonad
+import XMonad.Hooks.EwmhDesktops
 
 main = xmonad defaultConfig
 	{ terminal		= "urxvt"
 	, modMask		= mod4Mask
 	, borderWidth	= 3
+	, normalBorderColor = "#073642"
+	, focusedBorderColor = "#2aa198"
+	, handleEventHook = fullscreenEventHook
 	}
-
