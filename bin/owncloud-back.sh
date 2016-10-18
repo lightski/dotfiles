7fd3a9c99f8b68cd53a5754906a27cc2354e66c3
lightski <drleitzke@gmail.com>
2016-10-18 11:00:38 -0500

    Change owncloud-back.sh paths to absolute, ie replace ~ with /home/me

--- bin/owncloud-back.sh
+++ bin/owncloud-back.sh
@@ -2,13 +2,14 @@
 # copies important laptop files to owncloud in case of HDD fail
 #  NOTE removed projects dir due to size concern. Re-add later if you compress these first!
 # @TODO -delete entries older than $timeframe (3 months/ 1 year?)
-declare -a dirs=("~/Documents/iit/sem5" "~/Documents/wikis")
-dest="~/ownCloud/backup"
+declare -a dirs=("/home/daniel/Documents/iit/sem5" "/home/daniel/Documents/wikis")
+dest="/home/daniel/ownCloud/backup"
 date=`date "+%Y%m%d_%H%M%S"`
 
 # rsync everything in dirs. symlink if same as most recent
 #  for SOME reason -e returns false, but -L correctly sees file here. weird...
-if [ -L "$dest/latest" ]; then # normal backup. has latest symlink
+if [ -L "$dest/latest" ]; then 
+    # backup. only copy if file not in latest backup
 	for fld in "${dirs[@]}"
 	do
 		rsync -az --link-dest="$dest/latest" "$fld" "$dest/$date"
