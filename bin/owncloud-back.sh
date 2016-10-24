d875db55057c129378b422c9974e3ef2a1a4ddad
lightski <drleitzke@gmail.com>
2016-10-24 16:52:32 -0500

    Changes owncloud-back to run full backups monthly then incremental when monthly backup present. Adds steam-preload to help launch steam with libraries loaded.

--- bin/owncloud-back.sh
+++ bin/owncloud-back.sh
@@ -2,28 +2,36 @@
 # copies important laptop files to owncloud in case of HDD fail
 #  NOTE removed projects dir due to size concern. Re-add later if you compress these first!
 # @TODO -delete entries older than $timeframe (3 months/ 1 year?)
-#       -run full backup monthly then incremental otherwise (also fixes problem of deleting old)
-#           ie, change 'latest' to most-recent-full backup
 declare -a dirs=("/home/daniel/Documents/iit/sem5" "/home/daniel/Documents/wikis")
 dest="/home/daniel/ownCloud/backup"
 date=`date "+%Y%m%d_%H%M%S"`
-
 # rsync everything in dirs. symlink if same as most recent
 #  for SOME reason -e returns false, but -L correctly sees file here. weird...
-if [ -L "$dest/latest" ]; then 
-    # backup. only copy if file not in latest backup
-	for fld in "${dirs[@]}"
-	do
-		rsync -az --link-dest="$dest/latest" "$fld" "$dest/$date"
-	done
-	rm "$dest/latest"
+if [ -L "$dest/full-monthly" ]; then 
+    thismonth=${date:4:2}
+    monthsym=`ls -l | grep full-monthly`
+    lastmonth=${monthsym:65:2}
+    if [ $(($thismonth-$lastmonth)) -eq 0 ]; then
+        # backup diff, ie copy if unchanged from monthly backup.
+        for fld in "${dirs[@]}"
+        do
+            rsync -az --link-dest="$dest/full-monthly"\
+                "$fld" "$dest/$date"
+        done
+    else # new monthly backup
+        for fld in "${dirs[@]}"
+        do
+            rsync -az "$fld" "$dest/$date"
+        done
+        rm "$dest/full-monthly"
+        ln -s "$date" "$dest/full-monthly"
+    fi
 else # first time backup
 	for fld in "${dirs[@]}"
 	do
 		rsync -az "$fld" "$dest/$date"
 	done
+    ln -s "$date" "$dest/full-monthly" # create full-monthly link
 fi
 
-# recreate latest link
-ln -s "$date" "$dest/latest"
 
